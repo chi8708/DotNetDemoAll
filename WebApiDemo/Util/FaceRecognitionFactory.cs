eeb540454d64cc77ec5784d01139a795f9eb5971
chi8708 <983143463@qq.com>
2024-07-21 11:09:27 +0800

    升级.net8

---
+++ WebApiDemo/Util/FaceRecognitionFactory.cs
@@ -0,0 +1,32 @@
+﻿using FaceRecognitionDotNet;
+
+namespace WebApiDemo.Util
+{
+
+    public static class FaceRecognitionFactory
+    {
+        public static object o = new object();
+        public static FaceRecognition FaceRecognition = FaceRecognition.Create(Path.Combine(Environment.CurrentDirectory, "models"));
+        //static string _modelsFolder = Path.Combine(Environment.CurrentDirectory, "models");
+        //static FaceRecognition Instance
+        //{
+        //    get
+        //    {
+        //        if (FaceRecognition == null)
+        //        {
+        //            lock (o)
+        //            {
+        //                if (FaceRecognition == null)
+        //                {
+        //                    FaceRecognition = FaceRecognition.Create(_modelsFolder);
+        //                }
+
+        //            }
+        //        }
+
+        //        return FaceRecognition;
+        //    }
+        //}
+
+    }
+}
